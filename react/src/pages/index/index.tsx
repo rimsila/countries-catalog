5503e2cab29e0d115db07022df9a44eb5c3205b2
zila <rimsila.itc@gmail.com>
2023-09-21 23:41:49 +0700

    init catelog ui

--- react/src/pages/index/index.tsx
+++ react/src/pages/index/index.tsx
@@ -1,7 +1,36 @@
+import { Table, Input, Typography, Space, Card } from "antd";
+
+const { Text } = Typography;
+
 export default function App() {
   return (
     <div className="container mx-auto">
-      <h1>Country Catalog</h1>
+      <Card
+        bordered={false}
+        title={
+          <Space size="large">
+            <Text strong type="danger">
+              Country Catalog
+            </Text>
+            <Input
+              allowClear
+              placeholder="Search Country"
+              className="sm:w-[200px] w-full"
+            />
+          </Space>
+        }
+      >
+        <Table
+          columns={[]}
+          dataSource={[]}
+          scroll={{
+            scrollToFirstRowOnChange: true,
+            // bellow tablet will have auto scroll x
+            x: 767,
+            y: "60vh",
+          }}
+        />
+      </Card>
     </div>
   );
 }
