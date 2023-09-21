ceb04c2037b1a6bea570e1697e83226ad1c3cc7d
zila <rimsila.itc@gmail.com>
2023-09-21 23:44:11 +0700

    implement UI and integration API

---
+++ react/src/pages/index/columns.tsx
@@ -0,0 +1,79 @@
+import { Modal, Typography, Image, Space } from "antd";
+import { CountriesCatalogItem } from "./type";
+import { ColumnType } from "antd/es/table";
+
+const { Text, Link, Paragraph } = Typography;
+
+const DetailItem = ({
+  label,
+  value,
+}: {
+  label: string | number;
+  value?: string | number;
+}) => (
+  <Paragraph>
+    <Text strong>{label}:</Text> {value}
+  </Paragraph>
+);
+
+const handleDetailCountries = (record: CountriesCatalogItem) => {
+  const { altSpellings, area, population, capital, idd, name, flags } =
+    record || {};
+  Modal.info({
+    icon: null,
+    okText: "Close",
+    title: name.official,
+    content: (
+      <div className="flex flex-col">
+        <Image
+          src={flags.png}
+          className="w-full self-center py-4"
+          preview={false}
+        />
+        <DetailItem label="Country Area" value={area} />
+        <DetailItem label="Population" value={population} />
+        <DetailItem label="Capital" value={capital?.join(", ")} />
+        <DetailItem label="Calling Codes" value={idd.root} />
+        <DetailItem label="Alternative Country Name" value={altSpellings} />
+      </div>
+    ),
+  });
+};
+
+export const columns: ColumnType<CountriesCatalogItem>[] = [
+  {
+    title: "Flag",
+    dataIndex: "flags",
+    key: "flags",
+    render: (flags) => <Image src={flags.png} alt="flag" width={80} />,
+  },
+  {
+    title: "Country Name",
+    dataIndex: ["name", "official"],
+    sorter: (a, b) => a.name.official.localeCompare(b.name.official),
+    showSorterTooltip: true,
+    render: (_, record) => (
+      <Link type="warning" onClick={() => handleDetailCountries(record)}>
+        {_}
+      </Link>
+    ),
+  },
+  { title: "2 character Country Code", dataIndex: "cca2" },
+  { title: "3 character Country Code", dataIndex: "cca3" },
+  {
+    title: "Native Country Name",
+    dataIndex: ["name", "nativeName"],
+    render: (_, { name }) => {
+      return name.official;
+    },
+  },
+  {
+    title: "Alternative Country Name",
+    dataIndex: "altSpellings",
+    render: (altSpellings) => altSpellings.join(", "),
+  },
+  {
+    title: "Country Calling Codes",
+    dataIndex: ["idd", "root"],
+  },
+];
--- react/src/pages/index/index.tsx
+++ react/src/pages/index/index.tsx
@@ -1,8 +1,29 @@
+import { useState, useMemo } from "react";
+import { useRequest, useDebounce } from "ahooks";
 import { Table, Input, Typography, Space, Card } from "antd";
+import { getAllCountries } from "./service";
+import { columns } from "./columns";
 
 const { Text } = Typography;
 
 export default function App() {
+  const [searchText, setSearchText] = useState("");
+  const debouncedSearchValue = useDebounce(searchText.trim(), { wait: 500 });
+
+  const { data: allCountries, loading: loadingAllCountries } =
+    useRequest(getAllCountries);
+
+  const filteredData = useMemo(() => {
+    if (!Array.isArray(allCountries?.data)) return [];
+
+    //fuzzy filter
+    return allCountries?.data.filter((country) =>
+      country.name.official
+        .toLowerCase()
+        .includes(debouncedSearchValue.toLowerCase())
+    );
+  }, [allCountries, debouncedSearchValue]);
+
   return (
     <div className="container mx-auto">
       <Card
@@ -15,14 +36,21 @@ export default function App() {
             <Input
               allowClear
               placeholder="Search Country"
+              onChange={(e) => setSearchText(e.target.value)}
               className="sm:w-[200px] w-full"
             />
           </Space>
         }
       >
         <Table
-          columns={[]}
-          dataSource={[]}
+          rowKey={(item) => item.area}
+          loading={loadingAllCountries}
+          columns={columns}
+          dataSource={filteredData}
+          pagination={{
+            defaultPageSize: 25,
+            showTotal: (total) => `Total ${total}`,
+          }}
           scroll={{
             scrollToFirstRowOnChange: true,
             // bellow tablet will have auto scroll x
